8ec691457605a5982bce075c31d45029a726a42c
Darin <darindev1000@gmail.com>
2020-08-11 13:36:30 -0700

    Get working chart from imported script. Typescript doesn't reognize Plotly though

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import * as Plotly from 'plotly.js/dist/plotly.js';
 
 @Component({
   selector: 'app-graph',
@@ -10,6 +11,28 @@ export class GraphComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const trace1 = {
+      type: 'bar',
+      x: [1, 2, 3, 4],
+      y: [5, 10, 2, 8],
+      marker: {
+          color: '#C8A2C8',
+          line: {
+              width: 2.5
+          }
+      }
+    };
+
+    const data = [ trace1 ];
+
+    const layout = {
+      title: 'Responsive to window\'s size!',
+      font: {size: 18}
+    };
+
+    const config = {responsive: true};
+
+    Plotly.newPlot('myDiv', data, layout, config );
   }
 
 }
